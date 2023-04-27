13b8ef94c6782fc65ee85dc4d93b46474529661d
LilianBernot <lilian.bernot@gmail.com>
2023-04-26 21:48:45 -0300

    asks for a string and verifies that it has the good formgit add --all

--- index.js
+++ index.js
@@ -16,4 +16,14 @@
 const prompt=require("prompt-sync")({sigint:true}); 
   
 var codigo = prompt("Escribe el codigo de tu destinacion : ");
+
+const regex = /^[A-Z]{3}\d{2}$/; // 3 leters, 2 numbers and that's it
+
+
+var valid = regex.test(codigo);
+while(valid !== true){
+    console.log("Este codigo no es valido !");
+    codigo = prompt("   Intenta otra vez : ");
+    valid = regex.test(codigo);
+}
 console.log("Vamos a "+codigo+"!");
