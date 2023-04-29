6da625fb5217337b0c1a7a6431e911c5a0aa0113
LilianBernot <lilian.bernot@gmail.com>
2023-04-29 11:55:31 -0300

    asks for the path and name, verifies it exists

--- desafio_2/index.js
+++ desafio_2/index.js
@@ -1,6 +1,12 @@
 import { readLineAsync, readline, test_rotation } from "./input.js";
 import { modify_file, write_something } from "./file.js";
 import path from 'path';
+import * as fs from 'fs';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log("ESTE herramienta esta desarrollado para cifrar un archivo con la rotacion que quieres.");
 
@@ -14,6 +20,13 @@ console.log("ESTE herramienta esta desarrollado para cifrar un archivo con la ro
 
 let path_name = await readLineAsync("Escribe la ruta relativa del archivo: ");
 let input_name = await readLineAsync("Escribe el nombre del archivo: ");
+while (fs.existsSync(__dirname + path_name + input_name) !== true){
+    console.log('El archivo no existe.');
+    console.log('   Intenta otra vez:');
+    path_name = await readLineAsync("   Escribe la ruta relativa del archivo: ");
+    input_name = await readLineAsync("   Escribe el nombre del archivo: ");
+}
+console.log("El archivo a cifrar : " + path_name + input_name);
 readline.close();
 
 const parsedPath = path.parse(input_name);
